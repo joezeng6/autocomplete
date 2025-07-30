839738739c3a3ddcd5bc07c8c9509305013028a0
joe <joezeng6@gmail.com>
2025-07-30 10:51:32 -0700

    working

---
+++ app/search.tsx
@@ -0,0 +1,14 @@
+"use server";
+
+import fs from "fs";
+import path from "path";
+
+const filePath = path.join(process.cwd(), "public", "data", "wordlist.txt");
+const fileContents = fs.readFileSync(filePath, "utf8");
+
+export async function search(text: string): Promise<string[]> {
+  return fileContents
+    .split("\n")
+    .filter((word) => word.startsWith(text))
+    .slice(0, 10);
+}
